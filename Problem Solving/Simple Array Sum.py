101a4882b457bac52e61afa3af80cbc057af57ed
Simón Marín Giraldo <53051453+smaring1@users.noreply.github.com>
2019-08-30 15:13:55 -0500

    Folder creation

---
+++ Problem Solving/Simple Array Sum.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    #
+    # Write your code here.
+    #
+    cont = 0
+    for x in range (0,len(ar)):
+        cont += ar[x]
+    return cont
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
